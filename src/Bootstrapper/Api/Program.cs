ae7325ba612cae6a404f9ccf71303fcb3f387437
Rachid <rachid3alaoui@gmail.com>
2025-12-28 07:58:50 -0800

    Cleanup

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+// Configure the HTTP request pipeline
 app.Run();
