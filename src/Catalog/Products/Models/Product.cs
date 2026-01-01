d8a8b4b2c43a8b1b0b96ce01a842c8ce58a30403
Rachid <rachid3alaoui@gmail.com>
2026-01-01 22:12:14 +0100

    Add rich-domain model entity for Product

---
+++ src/Catalog/Products/Models/Product.cs
@@ -0,0 +1,41 @@
+namespace Catalog.Products.Models;
+
+public class Product : Entity<Guid>
+{
+    public string Name { get; private set; } = null!;
+    public List<string> Category { get; private set; } = [];
+    public string Description { get; private set; } = null!;
+    public string ImageFile { get; private set; } = null!;
+    public decimal Price { get; private set; }
+
+    public static Product Create(Guid id, string name, List<string> category, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            ImageFile = imageFile,
+            Price = price
+        };
+        
+        return product;
+    }
+
+    public void Update(string name, List<string> category, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        Name = name; 
+        Category =  category;
+        Description = description;
+        ImageFile = imageFile;
+        Price = price;
+        
+        // TODO : if price has change, rais ProductPriceChanged domain event
+    }
+}
