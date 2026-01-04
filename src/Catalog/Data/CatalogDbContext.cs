8904b841842a1fb31bdf7b42a1c3354c9908688e
Rachid <rachid3alaoui@gmail.com>
2026-01-04 22:28:06 +0100

    Init EF Core setup

---
+++ src/Catalog/Data/CatalogDbContext.cs
@@ -0,0 +1,13 @@
+namespace Catalog.Data;
+
+public class CatalogDbContext(DbContextOptions<CatalogDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("catalog");
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
--- src/Catalog/GlobalUsings.cs
+++ src/Catalog/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using Shared.DDD;
 global using Catalog.Products.Models;
 global using Catalog.Products.Events;
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore;
