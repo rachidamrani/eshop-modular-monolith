ea8a79db663b45224df388a7fdec3d8e4bda8889
Rachid <rachid3alaoui@gmail.com>
2026-01-10 20:15:56 +0100

    wip

--- src/Catalog/CatalogModule.cs
+++ src/Catalog/CatalogModule.cs
@@ -8,11 +8,22 @@ public static class CatalogModule
 {
     public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(connectionString));
+        
         return services;
     }
 
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
     {
+        InitializeDataBaseAsync(app).GetAwaiter().GetResult();
         return app;
+    }
+
+    private static async Task InitializeDataBaseAsync(IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+        await context.Database.MigrateAsync();
     }
 }
--- src/Catalog/GlobalUsings.cs
+++ src/Catalog/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using Catalog.Products.Models;
 global using Catalog.Products.Events;
 global using System.Reflection;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Catalog.Data;
